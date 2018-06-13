62562849f8169965f81ffc5fb24575ce948914c4
Jaskot <piotr.jaskot@nokia.com>
2018-06-13 15:56:04 +0200

    Add storing message id in session

--- note/views.py
+++ note/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CreateMessageForm
 import uuid
 from .redis import create_session
@@ -12,12 +12,17 @@ def index_view(request):
             message_body = form.cleaned_data['message_body']
             with create_session() as session:
                 session.set(message_id, message_body)
+            request.session['msg_id'] = message_id
+            return redirect('note/added')
+    else:
+        form = CreateMessageForm()
+    return render(request, 'index.html', {'form': form})
 
-    # just for debug purposes
+
+def note_added_view(request):
     db = dict()
     with create_session() as session:
         for i, x in enumerate(session.scan_iter()):
             db[i] = (x, session.get(x))
         print(db)
-
-    return render(request, 'index.html', {'db': db})
+    return render(request, 'note.html', {'msg_id': request.session['msg_id'], 'db': db})
