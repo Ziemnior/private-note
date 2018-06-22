a2914e5407e9cd4626f147ab16b9708b0c5e0735
Piotr <djpitugta@gmail.com>
2018-06-22 19:04:14 +0200

    Improve confirmation page behaviour

--- note/views.py
+++ note/views.py
@@ -23,22 +23,10 @@ def index_view(request):
     return render(request, 'index.html', {'form': form})
 
 
-def confirmation_context_helper(context):
-    with create_session() as session:
-        message = session.lrange(context['msg_id'], 0, -1)
-        return True if message else None
-
-
-@confirm_required('confirm.html', confirmation_context_helper)
+@confirm_required('confirm.html')
 def show_note_view(request, msg_id):
     with create_session() as session:
         msg_body_ciphered = session.lrange(msg_id, 0, -1)
-        try:
-            msg_body_ciphered[0]
-        except IndexError:
-            msg_body = 'message was destroyed already'
-        else:
-            session.delete(msg_id)
-            msg_body = Ciphering.decipher_message(msg_id.encode(), msg_body_ciphered)
-        finally:
-            return render(request, 'show_note.html', context={'msg_body': msg_body})
+        session.delete(msg_id)
+        msg_body = Ciphering.decipher_message(msg_id.encode(), msg_body_ciphered)
+    return render(request, 'show_note.html', context={'msg_body': msg_body})
--- privatenote/settings.py
+++ privatenote/settings.py
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-SESSION_ENGINE = "redis_sessions.session"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
