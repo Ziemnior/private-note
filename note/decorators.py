2f708668b212614c709f3aa19a6375ef7c9fc39b
Piotr <djpitugta@gmail.com>
2018-06-22 19:04:14 +0200

    Improve confirmation page behaviour

--- note/decorators.py
+++ note/decorators.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.utils.functional import wraps
+from .redis_utils import create_session
 
 
-def confirm_required(template_name, context_creator, key='__confirm__'):
+def confirm_required(template_name, key='__confirm__'):
     def decorator(func):
         def inner(request, *args, **kwargs):
-            if key in request.POST:
-                return func(request, *args, **kwargs)
-            else:
-                return render(request, template_name, {'msg_id': context_creator(kwargs)})
+            with create_session() as session:
+                if not session.lrange(kwargs['msg_id'], 0, -1):
+                    return render(request, 'show_note.html', {'msg_body': 'No such message (Or it was destroyed already)'})
+                else:
+                    if key in request.POST:
+                        return func(request, *args, **kwargs)
+                    else:
+                        return render(request, template_name, {})
         return wraps(func)(inner)
     return decorator
